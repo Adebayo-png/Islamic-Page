7b3f2f1e0326dbda452a4ebc965300cfe30dda8b
Adebayo-png <adebayomuhammad40@gmail.com>
2025-09-19 19:33:33 +1300

    Your message

---
+++ app.js
@@ -0,0 +1,18 @@
+    // When the page loads
+    window.addEventListener('load', function () {
+      // Set timeout for 5 minutes = 3,000 milliseconds
+      setTimeout(function () {
+        // Hide loading message
+        document.getElementById('loading').style.display = 'none';
+
+        document.getElementById('main-content').style.display = 'block';
+      },); // 5 minutes
+    });
+
+    let number = 0;
+
+    let numbe = 0;
+
+    let number3 = 0;
+
+    let number4 = 0;
